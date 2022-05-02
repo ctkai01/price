01eaeb2bd1210f1b5cff576c2528d04e678bbb3f
ctkai <quangnam11032001@gmail.com>
2022-05-02 23:16:07 +0700

    Init

--- change-price.php
+++ change-price.php
@@ -52,8 +52,8 @@ include('./database.php')
                 </div>
                 <div class='input-group'>
                     <span class='input-group-text'>".$row['name']."</span>
-                    <input type='text' name=".$row['id'].'mua_vao'." class='form-control' value=".$row['mua_vao'].">
-                    <input type='text' name=".$row['id'].'ban_ra'." class='form-control' value=".$row['ban_ra'].">
+                    <input type='text' name=".$row['id'].'mua_vao'." class='form-control number-separator' value=".$row['mua_vao'].">
+                    <input type='text' name=".$row['id'].'ban_ra'." class='form-control number-separator' value=".$row['ban_ra'].">
                 </div>
                 <input type='hidden' name=".$row['id'].'name'." value='".$row['name']."'>
                 <div class='line'></div>
@@ -69,6 +69,13 @@ include('./database.php')
     </form>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/gh/amiryxe/easy-number-separator/easy-number-separator.js"></script>
+    <script>
+        easyNumberSeparator({
+            selector: '.number-separator',
+            separator: '.'
+        })
+    </script>
 </body>
 
 </html>
